c1c7fc43c7e18a22058092f0865b85a534e3822d
Mega <daxaka.msk@gmail.com>
2016-03-03 09:48:37 +0300

    Resolve #1

--- button.js
+++ button.js
@@ -4,21 +4,21 @@ $('document').ready(function() {
 		composer.addButton('fa fa-eyedropper', function(textarea, selectionStart, selectionEnd) {
 			text = textarea;
 		});
-		$(document).on('click','.fa-eyedropper',function(){
+		$(document).on('click','[data-format="eyedropper"]',function(){
 			$('.fa-eyedropper').ColorPicker({
 				onShow: function (el) {
-					$(el).fadeIn(500);
+					$(el).show();
 					return false;
 					controls.updateTextareaSelection(text, start, end);
 				},
 				onHide: function (el) {
-					$(el).fadeOut(500);
+					$(el).hide();
 					return false;
 				},
 				onSubmit: function (hsb, hex, rgb, el) {
 					$(el).ColorPickerHide();
 					if(start === end){
-						controls.insertIntoTextarea(text, '%(#'+hex+')[Insert Text Here]');
+						controls.insertIntoTextarea(text, '%(#'+hex+')[Цветной текст]');
 						controls.updateTextareaSelection(text, start + 11, end + 27);
 					} else {
 						controls.wrapSelectionInTextareaWith(text, '%(#'+hex+')[',']');
