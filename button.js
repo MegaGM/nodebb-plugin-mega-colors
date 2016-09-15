b15c68d22044f4194baa812d5a05c5311859895f
Mega <Aza.Noriega@gmail.com>
2016-09-15 18:59:38 +0500

    fix wrong text selection

--- button.js
+++ button.js
@@ -1,10 +1,10 @@
-$('document').ready(function() {
-	require(['composer', 'composer/controls'], function(composer, controls) {
+$('document').ready(function () {
+	require(['composer', 'composer/controls'], function (composer, controls) {
 		var text, start, end;
-		composer.addButton('fa fa-eyedropper', function(textarea, selectionStart, selectionEnd) {
+		composer.addButton('fa fa-eyedropper', function (textarea, selectionStart, selectionEnd) {
 			text = textarea;
 		});
-		$(document).on('click','[data-format="eyedropper"]',function(){
+		$(document).on('click', '[data-format="eyedropper"]', function () {
 			$('.fa-eyedropper').ColorPicker({
 				onShow: function (el) {
 					$(el).show();
@@ -17,17 +17,17 @@ $('document').ready(function() {
 				},
 				onSubmit: function (hsb, hex, rgb, el) {
 					$(el).ColorPickerHide();
-					if(start === end){
-						controls.insertIntoTextarea(text, '%(#'+hex+')[Цветной текст]');
-						controls.updateTextareaSelection(text, start + 11, end + 27);
+					if (start === end) {
+						controls.insertIntoTextarea(text, '%(#' + hex + ')[Colored Text Here]');
+						controls.updateTextareaSelection(text, start + 11, end + 28);
 					} else {
-						controls.wrapSelectionInTextareaWith(text, '%(#'+hex+')[',']');
+						controls.wrapSelectionInTextareaWith(text, '%(#' + hex + ')[', ']');
 						controls.updateTextareaSelection(text, start + 11, end + 11);
 					}
 				}
 			}).trigger('click');
 		});
-		$(document).on('blur','textarea.write',function(){
+		$(document).on('blur', 'textarea.write', function () {
 			start = this.selectionStart;
 			end = this.selectionEnd;
 		});
